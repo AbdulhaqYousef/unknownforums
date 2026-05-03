1f15fbed8f61ec1ac1c2eb15a5af804511f38119
alexsan0x <alexsan0x@pm.me>
2026-05-03 20:17:36 +0300

    Improve registration failure feedback

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -14,6 +14,7 @@ class RegistrationsController < ApplicationController
       session[:return_to_after_email_otp] = root_path
       redirect_to email_otp_path, notice: "We sent a verification code to #{@user.email}."
     else
+      flash.now[:alert] = "Registration failed. Please fix the highlighted fields below."
       render :new, status: :unprocessable_entity
     end
   rescue EmailOtpSender::DeliveryDisabled, EmailOtpSender::DeliveryFailed, Net::SMTPError, IOError, Timeout::Error, SocketError => error
@@ -21,10 +22,12 @@ class RegistrationsController < ApplicationController
     @user.destroy if @user&.persisted? && !@user.email_verified?
     @user ||= User.new(registration_params)
     @user.errors.add(:base, "We could not send the verification email. Please try again.")
+    flash.now[:alert] = "Registration failed because the verification email could not be sent."
     render :new, status: :unprocessable_entity
   rescue ActiveRecord::RecordNotUnique
     @user ||= User.new(registration_params)
     @user.errors.add(:base, "That username or email is already taken.")
+    flash.now[:alert] = "Registration failed. That username or email is already taken."
     render :new, status: :unprocessable_entity
   end
 
